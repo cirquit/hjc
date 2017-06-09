15a4dd6340c71a67d3eee1e73ec8b81b73ddb3a7
Eugen <eugen@bopp.me>
2017-06-09 14:22:59 +0200

    fix IndexGet return type checking

--- src/TypeCheck/TypeCheck.hs
+++ src/TypeCheck/TypeCheck.hs
@@ -148,7 +148,7 @@ unify (IndexGet call ix) = do
     when (not defined) $ do
         appendError . return $ "operator [] is not defined for type \"" ++ showJC callType ++ "\", allowed types are \"" ++ DL.intercalate ", " (map showJC allowedTypes) 
     ix `shouldBeType_` IntT
-    return callType
+    return IntT
 
 unify (UnOp unop expr) = do
     case unop of
