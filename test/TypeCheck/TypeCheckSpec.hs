b9b9050fc3c52a50da35075a4c2773ab0ad40294
cirquit <alex.isenko@googlemail.com>
2017-06-13 17:43:00 +0200

    added inheritence tests, refactored tests

--- test/TypeCheck/TypeCheckSpec.hs
+++ test/TypeCheck/TypeCheckSpec.hs
@@ -4,310 +4,138 @@ import AST
 import ASTParser
 import TypeCheck.TypeCheck
 import qualified TypeCheck.TCCore as T
-import Output (showTE)
+import Output
+import Config
 
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import Control.Monad
 import Text.Megaparsec
 
 
 main :: IO ()
 main = hspec spec
 
+
+
+
+
 spec :: Spec
 spec = do
     describe "TypeCheck.typecheck" $ do
-        it "should have zero length of typescope errors in Add.java" $ do
-            let inputFile = "test/examples/Add.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
+        describe "should be successful" $ do
+            let showTypeErrors      = True
+                successfulTitle     = "should have zero length of typescope errors in "
+                successfulFileNames = [
+                      "test/examples/Add.java"
+                    , "test/examples/Arg.java"
+                    , "test/examples/Arithmetic.java"
+                    , "test/examples/BinarySearch.java"
+                    , "test/examples/Add.java"
+                    , "test/examples/BinarySearch.java"
+                    , "test/examples/BinaryTree.java"
+                    , "test/examples/BlockStatement.java"
+                    , "test/examples/Brainfuck.java"
+                    , "test/examples/BubbleSort.java"
+                    , "test/examples/Call.java"
+                    , "test/examples/CallThis.java"
+                    , "test/examples/Comparisons.java"
+                    , "test/examples/Extends.java"
+                    , "test/examples/Factorial.java"
+                    , "test/examples/Fields.java"
+                    , "test/examples/GameOfLifeAnim.java"
+                    , "test/examples/Hanoi.java"
+                    , "test/examples/If.java"
+                    , "test/examples/IntOps.java"
+                    , "test/examples/Leet.java"
+                    , "test/examples/LinearSearch.java"
+                    , "test/examples/LinkedList.java"
+                    , "test/examples/Loops.java"
+                    , "test/examples/Min.java"
+                    , "test/examples/QuickSort.java"
+                    , "test/examples/RaytraceAndGauss.java"
+                    , "test/examples/Raytrace.java"
+                    , "test/examples/Spill.java"
+                    , "test/examples/StackArgs.java"
+                    , "test/examples/TreeVisitor.java"
+                    ]
 
-        it "should have zero length of typescope errors in Arg.java" $ do
-            let inputFile = "test/examples/Arg.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
+                testList            = zip successfulFileNames (iterate id successfulTitle)
 
-        it "should have zero length of typescope errors in Arithmetic.java" $ do
-            let inputFile = "test/examples/Arithmetic.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
+            mapM_ (\(input, title) -> it (title ++ input) $ do
+                hasTypeErrors input showTypeErrors >>= \b -> b `shouldBe` False) testList
 
-        it "should have zero length of typescope errors in BinarySearch.java" $ do
-            let inputFile = "test/examples/BinarySearch.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
+            describe "inheritence tests should be successful" $ do
 
-        it "should have zero length of typescope errors in BinaryTree.java" $ do
-            let inputFile = "test/examples/BinaryTree.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
+                let successfulTitle     = "should have zero length of typescope errors in "
+                    successfulFileNames = [
+                          "test/examples/z_ShouldFail/Inheritance/EulerOO.java"
+                        , "test/examples/z_ShouldFail/Inheritance/NewtonOO.java"
+                        , "test/examples/z_ShouldFail/Inheritance/PrimesOO.java"
+                        , "test/examples/z_ShouldFail/Inheritance/QuickSortOO.java"
+                        , "test/examples/z_ShouldFail/Inheritance/TreeVisitor.java"
+                        ]    
+                    testList            = zip successfulFileNames (iterate id successfulTitle)
 
-        it "should have zero length of typescope errors in BlockStatement.java" $ do
-            let inputFile = "test/examples/BlockStatement.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Brainfuck.java" $ do
-            let inputFile = "test/examples/Brainfuck.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in BubbleSort.java" $ do
-            let inputFile = "test/examples/BubbleSort.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Call.java" $ do
-            let inputFile = "test/examples/Call.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in CallThis.java" $ do
-            let inputFile = "test/examples/CallThis.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Comparisons.java" $ do
-            let inputFile = "test/examples/Comparisons.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Extends.java" $ do
-            let inputFile = "test/examples/Extends.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Factorial.java" $ do
-            let inputFile = "test/examples/Factorial.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Fields.java" $ do
-            let inputFile = "test/examples/Fields.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in GameOfLifeAnim.java" $ do
-            let inputFile = "test/examples/GameOfLifeAnim.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Hanoi.java" $ do
-            let inputFile = "test/examples/Hanoi.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in If.java" $ do
-            let inputFile = "test/examples/If.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in IntOps.java" $ do
-            let inputFile = "test/examples/IntOps.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Leet.java" $ do
-            let inputFile = "test/examples/Leet.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in LinearSearch.java" $ do
-            let inputFile = "test/examples/LinearSearch.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in LinkedList.java" $ do
-            let inputFile = "test/examples/LinkedList.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Loops.java" $ do
-            let inputFile = "test/examples/Loops.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Min.java" $ do
-            let inputFile = "test/examples/Min.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in QuickSort.java" $ do
-            let inputFile = "test/examples/QuickSort.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors RaytraceAndGauss.java" $ do
-            let inputFile = "test/examples/RaytraceAndGauss.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Raytrace.java" $ do
-            let inputFile = "test/examples/Raytrace.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in Spill.java" $ do
-            let inputFile = "test/examples/Spill.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors in StackArgs.java" $ do
-            let inputFile = "test/examples/StackArgs.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
-
-        it "should have zero length of typescope errors TreeVisitor.java" $ do
-            let inputFile = "test/examples/TreeVisitor.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            showErrors typescope
-            (length $ T._errors typescope) `shouldBe` 0
+                mapM_ (\(input, title) -> it (title ++ input) $ do
+                    hasTypeErrors input showTypeErrors >>= \b -> b `shouldBe` False) testList
 
 
-        it "should have typescope errors MethodCallWrong.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/MethodCallWrong.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
 
-        it "should have typescope errors MethodWronglySpelled.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/MethodWronglySpelled.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
+        describe "failed examples" $ do
+            let showTypeErrors      = False
+                successfulTitle     = "should have some typescope errors in "
+                successfulFileNames = [
+                      "test/examples/z_ShouldFail/TypeErrors/MethodCallWrong.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/MethodWronglySpelled.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/LinkedListBUG.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/PrintFirstArg.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/Shadow.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/TooManyArguments.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/TypeOfArgWrong.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/UndeclaredType.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/UndeclaredVar1.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/UndeclaredVar2.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/WrongArgumentTypes.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/WrongArgumentTypes2.java"
+                    , "test/examples/z_ShouldFail/TypeErrors/WrongTypeDeclared.java"
+                    ]
 
-        it "should have typescope errors LinkedListBUG.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/LinkedListBUG.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
+                testList            = zip successfulFileNames (iterate id successfulTitle)
 
-        it "should have typescope errors PrintFirstArg.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/PrintFirstArg.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
+            mapM_ (\(input, title) -> it (title ++ input) $ do
+                hasTypeErrors input showTypeErrors >>= \b -> b `shouldBe` True) testList
 
-        it "should have typescope errors Shadow.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/Shadow.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
 
-        it "should have typescope errors TooManyArguments.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/TooManyArguments.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
 
-        it "should have typescope errors TypeOfArgWrong.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/TypeOfArgWrong.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
+testConfig :: Config
+testConfig = Config
+    {
+      parse'        = True
+    , showAst'      = False
+    , showResult'   = False
+    , showTime'     = True
+    , compileToCmm  = True 
+    , javaOutputDir = "../output"
+    , cmmOutputDir  = "../cmm-output"
+    , typeErrLvl   = AllErrors -- FirstError -- Silently 
+    }
 
-        it "should have typescope errors UndeclaredType.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/UndeclaredType.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
-
-        it "should have typescope errors UndeclaredVar1.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/UndeclaredVar1.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
-
-        it "should have typescope errors UndeclaredVar2.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/UndeclaredVar2.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
-
-        it "should have typescope errors WrongArgumentTypes.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/WrongArgumentTypes.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
-
-        it "should have typescope errors WrongArgumentTypes2.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/WrongArgumentTypes2.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
-
-        it "should have typescope errors WrongTypeDeclared.java" $ do
-            let inputFile = "test/examples/z_ShouldFail/TypeErrors/WrongTypeDeclared.java"
-            eres <- parseFile inputFile
-            typescope <- checkType eres
-            (0 < (length $ T._errors typescope)) `shouldBe` True
-
-parseFile inputFile = do
+hasTypeErrors :: String -> Bool -> IO Bool
+hasTypeErrors inputFile showTypeErrors = do
     input <- readFile inputFile
     let eres = runParser testParser inputFile input
-    return eres
+    case eres of
+        (Left errors) -> do
+            let oi = failed inputFile input (-1) errors
+            showFailure oi testConfig
+            return False
+        (Right ast)   -> do
+            typescope <- typecheck ast
+            when showTypeErrors $ showErrors typescope
+            return $ (0 < (length $ T._errors typescope))
 
-checkType (Right ast) = do
-    typescope <- typecheck ast
-    return typescope
 
 showErrors :: T.TypeScope -> IO ()
 showErrors typescope = do
---
+++ test/examples/z_ShouldFail/Inheritance/QuickSortOO.java
@@ -0,0 +1,122 @@
+// variant of quicksort, using sub-classes and arrays as parameters
+
+class QuickSortOO {
+    public static void main(String[] a){
+	System.out.println(new TestQS().Start(10));
+    }
+}
+
+// This class contains the array of integers and
+// methods to initialize, print and sort the array
+// using Quicksort
+class QS {
+    int maxlen; // maximum over all right params seen in calls
+
+    // Sort array of integers using Quicksort method
+    public int Sort(int left, int right, int [] number){
+	int v ;
+	int i ;
+	int j ;
+	int nt;
+	int t ;
+	boolean cont01;
+	boolean cont02;
+	int aux03 ;
+        if (maxlen < right) maxlen = right ; 
+        else maxlen = maxlen; 
+	t = 0 ;
+	if (left < right){
+	    v = number[right] ;
+	    i = left - 1 ;
+	    j = right ;
+	    cont01 = true ;
+	    while (cont01){
+		cont02 = true ;
+		while (cont02){
+		    i = i + 1 ;
+		    aux03 = number[i] ;
+		    if (!(aux03<v)) cont02 = false ;
+		    else cont02 = true ;
+		}
+		cont02 = true ;
+		while (cont02){
+		    j = j - 1 ;
+		    aux03 = number[j] ;
+		    if (!(v < aux03)) cont02 = false ;
+		    else cont02 = true ;
+		}
+
+
+		t = number[i] ;
+		number[i] = number[j] ;
+		number[j] = t ;
+		//aux03 = i + 1 ;
+		if ( j < (i+1)) cont01 = false ;
+		else cont01 = true ;
+	    }
+	    number[j] = number[i] ;
+	    number[i] = number[right] ;
+	    number[right] = t ;
+	    nt = this.Sort(left,i-1,number);
+	    nt = this.Sort(i+1,right,number);
+	}
+	else nt = 0 ;
+	return 0 ;
+    }
+
+    
+}
+
+class TestQS extends QS {
+    int[] number ;
+    int size ;
+
+     int indent;
+    // Print array of integers
+    public int Print(){
+	int j ;
+	j = 0 ;
+	while (j < (size)) {
+	    System.out.println(number[j]);
+	    j = j + 1 ;
+	}
+	return 0 ;
+    }
+
+    // Initialize array of integers
+    public int Init(int sz){
+	size = sz ;
+	number = new int[sz] ;
+	
+	number[0] = 20 ;
+	number[1] = 7  ; 
+	number[2] = 12 ;
+	number[3] = 18 ;
+	number[4] = 2  ; 
+	number[5] = 11 ;
+	number[6] = 6  ; 
+	number[7] = 9  ; 
+	number[8] = 19 ; 
+	number[9] = 5  ;
+	
+	return 0 ;	
+    }
+
+    // Invoke the Initialization, Sort and Printing
+    // Methods
+    public int Start(int sz){
+	int aux01 ;
+	maxlen = 0;                        // field in superclass!
+	aux01 = this.Init(sz);
+	aux01 = this.Print();
+	System.out.println(9999);
+	aux01 = size - 1 ;
+	aux01 = this.Sort(0,aux01,number); // uses method in a superclass
+	aux01 = this.Print();
+	System.out.println(9999);
+	System.out.println(maxlen);        // print length
+	return 0 ;
+    }
+
+}
+
